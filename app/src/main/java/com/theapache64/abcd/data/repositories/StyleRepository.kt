e40cc88690611d40f3320e7d745e88e3ac676816
theapache64 <theapache64@gmail.com>
2019-06-26 15:49:43 +0530

    🌟 add local style and art styles

--- app/src/main/java/com/theapache64/abcd/data/repositories/StyleRepository.kt
+++ app/src/main/java/com/theapache64/abcd/data/repositories/StyleRepository.kt
@@ -1,13 +1,12 @@
 package com.theapache64.abcd.data.repositories
 
-import com.theapache64.abcd.models.Server
+import com.theapache64.abcd.R
 import com.theapache64.abcd.models.Style
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class StyleRepository @Inject constructor(
-    server: Server
 ) {
 
     companion object {
@@ -16,40 +15,40 @@ class StyleRepository @Inject constructor(
     }
 
     val styles = listOf(
-        Style("Random", CODE_RANDOM, server, "random"),
-        Style("Nightfall", "0", server, "0"),
-        Style("Highway", "1", server, "1"),
-        Style("Loch", "2", server, "2"),
-        Style("Dusk", "3", server, "3"),
-        Style("Twilight", "4", server, "4"),
-        Style("Dawn", "5", server, "5"),
-        Style("Marine", "6", server, "6"),
-        Style("Lagoon", "7", server, "7"),
-        Style("Reflection", "8", server, "8"),
-        Style("Dark", "9", server, "9"),
-        Style("Eve", "10", server, "10")
+        Style("Random", R.drawable.style_random, CODE_RANDOM),
+        Style("Nightfall", R.drawable.style_0, "0"),
+        Style("Highway", R.drawable.style_1, "1"),
+        Style("Loch", R.drawable.style_2, "2"),
+        Style("Dusk", R.drawable.style_3, "3"),
+        Style("Twilight", R.drawable.style_4, "4"),
+        Style("Dawn", R.drawable.style_5, "5"),
+        Style("Marine", R.drawable.style_6, "6"),
+        Style("Lagoon", R.drawable.style_7, "7"),
+        Style("Reflection", R.drawable.style_8, "8"),
+        Style("Dark", R.drawable.style_9, "9"),
+        Style("Eve", R.drawable.style_10, "10")
     )
 
     val artStyles = listOf(
-        Style("None", CODE_NONE, server, "none"),
-        Style("Candy", "candy", server, "candy"),
-        Style("Mosaic", "mosaic", server, "mosaic"),
-        Style("Rain Princess", "rain-princess-cropped", server, "rain"),
-        Style("Udnie", "udnie", server, "udnie"),
-        Style("Wassily", "art0", server, "0"),
-        Style("Turcato", "art1", server, "1"),
-        Style("Asheville", "art2", server, "2"),
-        Style("Mathias", "art3", server, "3"),
-        Style("WWH", "art4", server, "4"),
-        Style("La Muse", "art5", server, "5"),
-        Style("Self", "art6", server, "6"),
-        Style("En Campo", "art7", server, "7")
+        Style("None", R.drawable.style_random, CODE_NONE),
+        Style("Candy", R.drawable.art_candy, "candy"),
+        Style("Mosaic", R.drawable.art_mosaic, "mosaic"),
+        Style("Rain Princess", R.drawable.art_rain_princess_cropped, "rain"),
+        Style("Udnie", R.drawable.art_udnie, "udnie"),
+        Style("Wassily", R.drawable.art_0, "0"),
+        Style("Turcato", R.drawable.art_1, "1"),
+        Style("Asheville", R.drawable.art_2, "2"),
+        Style("Mathias", R.drawable.art_3, "3"),
+        Style("WWH", R.drawable.art_4, "4"),
+        Style("La Muse", R.drawable.art_5, "5"),
+        Style("Self", R.drawable.art_6, "6"),
+        Style("En Campo", R.drawable.art_7, "7")
     )
 
     fun getNoArtStyle(): Style {
         var noneArtStyle: Style? = null
         for (style in artStyles) {
-            if (style.code == CODE_NONE) {
+            if (style.apiCode == CODE_NONE) {
                 noneArtStyle = style
                 break
             }
--- app/src/main/java/com/theapache64/abcd/models/Style.kt
+++ app/src/main/java/com/theapache64/abcd/models/Style.kt
@@ -1,19 +1,10 @@
 package com.theapache64.abcd.models
 
-import com.theapache64.abcd.data.repositories.StyleRepository
+import androidx.annotation.DrawableRes
 import java.io.Serializable
 
 open class Style(
     val name: String,
-    val code: String,
-    val server: Server,
-    val apiCode : String
-) : Serializable {
-
-
-    val imageUrl = if (code == StyleRepository.CODE_RANDOM)
-    // Random image url
-        "https://i.imgur.com/gin0rqw.png"
-    else
-        "http://${server.ip}/styles/$code.jpg"
-}
+    @DrawableRes val image: Int,
+    val apiCode: String
+) : Serializable
